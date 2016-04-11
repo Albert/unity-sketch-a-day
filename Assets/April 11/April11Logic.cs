8bd144ea70462d91c7eba3eefb4ac2acb6d6d42d
Albert Hwang <hwang.al@gmail.com>
2016-04-11 15:11:48 -0400

    april 11, spinning top

---
+++ Assets/April 11/April11Logic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April11Logic : MonoBehaviour {
+	public Rigidbody rbody;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		rbody.AddTorque (Vector3.up * 50000000000000);
+	}
+	
+}
