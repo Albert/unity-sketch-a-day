a06cbb68180ff3ffcd7d22095ec437b731f10378
Albert Hwang <hwang.al@gmail.com>
2016-04-17 20:18:57 -0400

    bounce like popping

---
+++ Assets/April 16/April16Logic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April16Logic : MonoBehaviour {
+	private Rigidbody rbody;
+	private SpringJoint[] sJoints;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		sJoints = GetComponents<SpringJoint> ();
+		foreach (SpringJoint sJoint in sJoints) {
+			sJoint.damper = 10;
+			sJoint.spring = 10;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			//transform.LookAt (Random.onUnitSphere);
+			rbody.AddTorque(Random.onUnitSphere * Random.Range(100, 500));
+		}
+	}
+}
