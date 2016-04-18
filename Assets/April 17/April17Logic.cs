9b570aadab79769ced35611912b87609021eab22
Albert Hwang <hwang.al@gmail.com>
2016-04-17 21:44:04 -0400

    compression box

---
+++ Assets/April 17/April17Logic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April17Logic : MonoBehaviour {
+	public GameObject prefab;
+	public GameObject top;
+	private Collider topCollider;
+
+	void Start () {
+		topCollider = top.GetComponent<Collider> ();
+	}
+	
+	void Update () {
+		GameObject s = Instantiate (prefab, Vector3.up * 5, Quaternion.identity) as GameObject;
+		s.GetComponent<Rigidbody> ().velocity = Vector3.down * 10;
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			topCollider.enabled = !topCollider.enabled;
+		}
+	}
+}
---
+++ Assets/April 17/April17LogicBall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April17LogicBall : MonoBehaviour {
+	void Start() {
+		GetComponent<Renderer> ().material.color = Random.ColorHSV();
+	}
+	void Update () {
+		if (transform.position.y < -1) {
+			Destroy (gameObject);
+		}
+	}
+}
