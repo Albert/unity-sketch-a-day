7c5432e0279b0e901cfb25f82b155d5abff92ecd
Albert Hwang <hwang.al@gmail.com>
2016-04-24 10:40:25 -0400

    spinning holding hands

---
+++ Assets/April24Logic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April24Logic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.up, 0.03f);
+	}
+}
