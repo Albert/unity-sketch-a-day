8c15ca11d7709d553e9b74fcad6b867072d43a06
Albert Hwang <hwang.al@gmail.com>
2016-04-27 00:46:25 -0400

    teetering cylinders

---
+++ Assets/April 25/April25Logic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April25Logic : MonoBehaviour {
+	public GameObject prefab;
+	public GameObject lightObj;
+	private int perDim = 10;
+	private int halfPerDim;
+
+	void Start () {
+		halfPerDim = perDim / 2;
+		for (int i = 0; i < perDim; i++) {
+			for (int j = 0; j < perDim; j++) {
+				for (int k = 0; k < perDim; k++) {
+					Instantiate (prefab, new Vector3 (i - halfPerDim, j - halfPerDim, k - halfPerDim), Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+	void Update () {
+		float x = Mathf.Sin (Time.time) * halfPerDim;
+		float y = Mathf.Cos (Time.time * 1.3f) * halfPerDim;
+		float z = Mathf.Sin (Time.time + 123) * halfPerDim;
+		lightObj.transform.position = new Vector3 (x, y, z);
+	}
+}
---
+++ Assets/April 26/April26Logic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class April26Logic : MonoBehaviour {
+	public GameObject prefab;
+	private List<GameObject> pieces;
+	private int length = 80;
+
+	void Start () {
+		pieces = new List<GameObject> ();
+		for (int i = 0; i < length; i++) {
+			GameObject newPiece = Instantiate (prefab, new Vector3 (i, 0, 0), Quaternion.identity) as GameObject;
+			pieces.Add (newPiece);
+		}
+	}
+	
+	void Update () {
+		for (int i = 0; i < length; i++) {
+			GameObject piece = pieces [i];
+			float rotMag = Mathf.Sin (Time.time + (float) i * 0.2f) * 90;
+			piece.transform.rotation = Quaternion.Euler (Vector3.right * rotMag);
+		}
+	}
+}
