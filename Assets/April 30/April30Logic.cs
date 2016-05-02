818d891c193f9cb4aaecab86b993436064341390
Albert Hwang <hwang.al@gmail.com>
2016-05-01 21:10:04 -0700

    april 30 -- all done

---
+++ Assets/April 30/April30Logic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class April30Logic : MonoBehaviour {
+	public GameObject[] eyes;
+	public GameObject subject;
+	public GameObject head;
+
+	void Update () {
+		foreach (GameObject eye in eyes) {
+			eye.transform.LookAt (subject.transform);
+		}
+
+		float x = Mathf.Sin (Time.time);
+		float y = Mathf.Cos (Time.time);
+		subject.transform.position = new Vector3 (x, y, 5);
+		subject.transform.LookAt (head.transform);
+	}
+}
